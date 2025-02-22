00c3a44c9e7f656ddfa14c6c601cd72440efc3e7
Euclides Brasil <euclides_brasil@hotmail.com>
2025-02-22 00:56:47 -0300

    feaure(testes): Adicionado teste de stress, simulando 50 requisições por segundo

---
+++ test/ArquiteturaDesafio.Test/Stress/StressTests.cs
@@ -0,0 +1,113 @@
+﻿using ArquiteturaDesafio.Application.UseCases.Commands.Transaction.CreateTransaction;
+using ArquiteturaDesafio.Core.Application.UseCases.Commands.AuthenticateUser;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArquiteturaDesafio.Test.Integration
+{
+    public class StressTest
+    {
+        private readonly HttpClient _client;
+        private readonly JsonSerializerOptions _options;
+
+        public StressTest()
+        {
+            _client = new HttpClient(new SocketsHttpHandler
+            {
+                PooledConnectionLifetime = TimeSpan.FromMinutes(2),
+                PooledConnectionIdleTimeout = TimeSpan.FromSeconds(30),
+                MaxConnectionsPerServer = 2000
+            });
+            _client.BaseAddress = new Uri("http://localhost:5000");
+
+            _options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
+        private async Task<string> GetAuthTokenAsync()
+        {
+            var command = new AuthenticateUserRequest
+            {
+                Username = "admin",
+                Password = "s3nh@"
+            };
+
+            var content = new StringContent(JsonSerializer.Serialize(command), Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("/auth/login", content);
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+            var responseObject = JsonSerializer.Deserialize<AuthenticateUserResult>(responseString, _options);
+
+            return responseObject?.Token;
+        }
+
+        [Fact]
+        public async Task StressSimulation()
+        {
+            double disponibility = 0.0;
+            var token = await GetAuthTokenAsync();
+            Assert.NotNull(token);
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var successCount = 0;
+            var errorCount = 0;
+            var random = new Random();
+            var iteration = 0;
+            try
+            {
+                while (iteration < 30)
+                {
+                    decimal randomDecimal = 1 + (decimal)(random.NextDouble() * (1000 - 1));
+                    for (var i = 1; i <= 50; i++)
+                    {
+                        var request = new CreateTransactionRequest
+                        {
+                            Amount = new Core.Application.UseCases.DTOs.MoneyDTO(randomDecimal),
+                            Description = $"Teste Randomico {Guid.NewGuid()}",
+                            Type = Core.Domain.Enum.TransactionType.Credit
+                        };
+                        request.UpdateDate(DateTime.UtcNow.Date);
+
+                        var content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+
+                        try
+                        {
+                            var response = await _client.PostAsync("/transaction", content);
+                            if (response.IsSuccessStatusCode)
+                            {
+                                System.Threading.Interlocked.Increment(ref successCount);
+                            }
+                            else
+                            {
+                                System.Threading.Interlocked.Increment(ref errorCount);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            System.Threading.Interlocked.Increment(ref errorCount);
+                        }
+                    }
+
+                    await Task.Delay(1000); // Espera de 1 segundo para a próxima rodada de 50 requisições
+                    iteration++;
+                }
+                disponibility = (successCount + errorCount) * 0.95;
+                Assert.True(successCount >= disponibility, $"Nem todas as requisições foram bem-sucedidas. Sucessos: {successCount} de {(successCount + errorCount)}");
+            }
+            catch (Exception ex)
+            {
+                disponibility = (successCount + errorCount) * 0.95;
+                Assert.True(successCount >= disponibility, $"Nem todas as requisições foram bem-sucedidas. Sucessos: {successCount} de {(successCount + errorCount)}; ERRO: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ test/ArquiteturaDesafio.Test/Stress/TransactionStressTest .cs
@@ -0,0 +1,103 @@
+﻿using NBomber;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Http.CSharp;
+using Xunit;
+using static System.Net.WebRequestMethods;
+
+namespace xUnitExample;
+
+public class LoadTestExample
+{
+    [Fact]
+    public void SimpleHttpExample()
+    {
+        using var httpClient = new HttpClient();
+
+        var scenario = Scenario.Create("http_scenario", async context =>
+        {
+            var step1 = await Step.Run("step_1", context, async () =>
+            {
+                var request =
+                    CreateRequest("GET", "https://nbomber.com")
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(new StringContent("{ some JSON }"));
+
+                var response = await Http.Send(httpClient, request);
+
+                return response;
+            });
+
+            var step2 = await Step.Run("step_2", context, async () =>
+            {
+                var request =
+                    Http.CreateRequest("GET", "https://nbomber.com")
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(new StringContent("{ some JSON }"));
+
+                var response = await Http.Send(httpClient, request);
+
+                return response;
+            });
+
+            return Response.Ok();
+        })
+        .WithoutWarmUp()
+        .WithLoadSimulations(Simulation.Inject(rate: 10, interval: TimeSpan.FromSeconds(1), TimeSpan.FromSeconds(10)));
+
+        var result = NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+
+        // StatsHelper API extensions provides: Get, Find, Exist methods to work with ScenarioStats, StepStats, StatusCodes, etc.
+
+        // gets scenario stats:
+        // it throws exception if "http_scenario" is not found
+        var scnStats = result.ScenarioStats.Get("http_scenario");
+
+        // finds scenario stats:
+        // it returns null if "http_scenario" is not found
+        scnStats = result.ScenarioStats.Find("http_scenario");
+
+        var step1Stats = scnStats.StepStats.Get("step_1");
+
+        var isStep2Exist = scnStats.StepStats.Exists("step_2");
+        var step2Stats = scnStats.StepStats.Get("step_2");
+
+        Assert.True(result.AllBytes > 0);
+        Assert.True(result.AllRequestCount > 0);
+        Assert.True(result.AllOkCount > 0);
+        Assert.True(result.AllFailCount == 0);
+
+        Assert.True(scnStats.Ok.Request.RPS > 0);
+        Assert.True(scnStats.Ok.Request.Count > 0);
+
+        // success rate 100% of all requests
+        Assert.True(scnStats.Ok.Request.Percent == 100);
+        Assert.True(scnStats.Fail.Request.Percent == 0);
+
+        Assert.True(scnStats.Ok.Latency.MinMs > 0);
+        Assert.True(scnStats.Ok.Latency.MaxMs > 0);
+
+        Assert.True(scnStats.Fail.Request.Count == 0);
+        Assert.True(scnStats.Fail.Latency.MinMs == 0);
+
+        Assert.True(step1Stats.Ok.Latency.Percent50 > 0);
+        Assert.True(step1Stats.Ok.Latency.Percent75 > 0);
+        Assert.True(step1Stats.Ok.Latency.Percent99 > 0);
+
+        // less than 5% error responses with status code 503
+        Assert.True(
+            step1Stats.Fail.StatusCodes.Exists("503")
+            && step1Stats.Fail.StatusCodes.Get("503").Percent < 5
+        );
+        // or you can use .Find() which may return null
+        Assert.True(step1Stats.Fail.StatusCodes.Find("503")?.Percent < 5);
+
+        Assert.True(step2Stats.Ok.DataTransfer.MinBytes > Bytes.FromKb(1));
+        Assert.True(step2Stats.Ok.DataTransfer.MaxBytes > Bytes.FromKb(1));
+        Assert.True(step2Stats.Ok.DataTransfer.Percent99 > 0);
+
+        Assert.True(step2Stats.Ok.DataTransfer.AllBytes < Bytes.FromGb(10));
+    }
+}
